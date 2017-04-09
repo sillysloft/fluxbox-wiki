9067d22221c898e4c0f343719cfb2895e9f9b49d
sillyslux <sillyslux@net.hr>
2017-04-10 00:42:47 +0200

    add custom components Navigation, Footer, LanguageMenu, ...

---
+++ custom/Footer.jsx
@@ -0,0 +1,28 @@
+/* global window */
+import React from 'react'
+import { prefixLink } from 'gatsby-helpers'
+
+import { pages } from 'config'
+
+module.exports = React.createClass({
+  propTypes () {
+    return {
+      children: React.PropTypes.any,
+    }
+  },
+  openEditWindow (evt) {
+    evt.preventDefault()
+    window.open('/edit/', '_blank', 'toolbar=no,menubar=no,titlebar=no,height=600,width=800')
+  },
+  render () {
+    const page = pages.filter(p => p.path).find(p => [p.path, prefixLink(p.path)].includes(this.props.location))||{ file: '', requirePath: '' }
+    // debugger
+    return (
+      <div className="footer">
+        fluxbox-wiki.org
+        <a style={{ float: 'right' }} href={`https://github.com/sillyslux/fluxbox-wiki/blob/master/pages/${page.requirePath}`}>edit at github</a>
+        <a href="eddy" onClick={this.openEditWindow}>eddy</a>
+      </div>
+    )
+  },
+})
---
+++ custom/Navigation.jsx
@@ -0,0 +1,60 @@
+/* global window */
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
+import Headroom from 'react-headroom'
+import { Navbar, Nav, NavItem, InputGroup, Button, Glyphicon, FormGroup, FormControl } from 'react-bootstrap-externaljs'
+import LinkContainer from 'react-router-bootstrap/lib/LinkContainer'
+import getPageProps from 'utils/pageInfo'
+import PropTypes from 'prop-types'
+
+import 'css/headroom.sass'
+import 'css/navbar.sass'
+import 'css/subnav-animation.sass'
+
+import LanguageMenu from './LanguageMenu'
+
+const NavItemRouted = item => (
+  <LinkContainer to={item.link}>
+    <NavItem eventKey={item.link}>{item.text}</NavItem>
+  </LinkContainer>
+)
+
+export default class Navigation extends Component {
+  render () {
+    const { lang } = getPageProps(this.props.location)
+    return (
+      <Headroom>
+        <Navbar inverse>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to={prefixLink('/')} className="text-muted">
+                fluxbox<span className="navbar-logo" />wiki
+              </Link>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Navbar.Form pullRight>
+            <FormGroup bsSize="sm" id="duck-search">
+              <InputGroup bsSize="sm" className="animated-input slideUp">
+                <FormControl type="text" />
+              </InputGroup>
+              <Button bsStyle="primary" bsSize="sm">
+                <Glyphicon glyph="search" />
+              </Button>
+            </FormGroup>
+          </Navbar.Form>
+          <Nav bsStyle="pills" pullRight>
+            <NavItemRouted link={prefixLink(`/${lang}/wiki/`)} text="Users" />
+            <NavItemRouted link={prefixLink(`/${lang}/devel/`)} text="Developers" />
+            <LanguageMenu location={this.props.location} />
+          </Nav>
+        </Navbar>
+      </Headroom>
+    )
+  }
+}
+
+Navigation.displayName = 'NavigationWikiPage'
+Navigation.propTypes = {
+  location: PropTypes.string.isRequired,
+}
