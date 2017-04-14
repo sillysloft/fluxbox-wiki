1334e6e2f3eed4aae0fe48a276d5cd919892cedd
sillyslux <sillyslux@net.hr>
2017-04-14 03:28:56 +0200

    rename pages/wiki-portal to pages/WikiPortal

--- pages/WikiPortal/_template.jsx
+++ pages/WikiPortal/_template.jsx
@@ -8,7 +8,9 @@ import { Grid, Col, Nav, NavItem, ListGroup, ListGroupItem } from 'react-bootstr
 import pageInfo from 'utils/pageInfo'
 import { LinkContainer } from 'react-router-bootstrap'
 
-// import 'css/sidebar-left.sass'
+import PageIndex from 'custom/PageIndex'
+
+import styles from 'css/wiki-portal.module.sass'
 
 const NavItemRouted = item => (
   <LinkContainer to={item.link}>
@@ -16,37 +18,22 @@ const NavItemRouted = item => (
   </LinkContainer>
 )
 
-let location
 
 export default class WikiPortal extends Component {
   render () {
-    location = this.props.location.pathname
-    const { page, language } = pageInfo(location)
+    const { page, language } = pageInfo(this.props.location.pathname)
 
-    const indexedPages = pages.filter(a => /wiki\/.+\/.?/.test(a.requirePath)&&a.file.name===language)
-    .sort((a, b) => a.data.title.localeCompare(b.data.title))
-    const index = page.file.dir === 'wiki-portal/Index' ? (
-      <ListGroup style={{ marginLeft: 0 }}>
-        {indexedPages.map(a => (
-          <ListGroupItem key={a.path}>
-            <Link to={prefixLink(a.path)}>
-              {a.data.title}
-            </Link>
-          </ListGroupItem>
-        ))}
-      </ListGroup>
-    ) : ''
     const navPages = {}
-    const regex = new RegExp(`^wiki-portal/.+/${language}.md`)
+    const regex = new RegExp(`^WikiPortal/.+/${language}.md`)
     pages.filter(a => regex.test(a.requirePath))
       .forEach((p) => {
         const name = p.requirePath.split('/')[1]
         navPages[name]= { path: p.data.path, title: p.data.title }
       })
     return (
-      <Grid>
-        <Col xs={3} lg={2} className="sidebar sidebar-left">
-          <Nav id="wiki-start" bsStyle="pills" stacked>
+      <Grid className={styles['wiki-portal']}>
+        <Col xs={3} lg={2} className="sidebar">
+          <Nav id="wiki-portal-navigation" bsStyle="pills" stacked>
             <NavItemRouted link={prefixLink(navPages.Entry.path)} text={navPages.Entry.title} />
             <NavItemRouted link={prefixLink(navPages.Index.path)} text={navPages.Index.title} />
             <NavItemRouted link={prefixLink(navPages.About.path)} text={navPages.About.title} />
@@ -57,9 +44,9 @@ export default class WikiPortal extends Component {
             <NavItemRouted link={prefixLink(navPages['Getting-Help'].path)} text={navPages['Getting-Help'].title} />
           </Nav>
         </Col>
-        <Col xs={9} lg={10} className="wiki-portal">
+        <Col xs={9} lg={10} className="content">
           {this.props.children}
-          {index}
+          {page.file.dir === 'WikiPortal/Index' ? <PageIndex location={this.props.location} /> : ''}
         </Col>
       </Grid>
     )
