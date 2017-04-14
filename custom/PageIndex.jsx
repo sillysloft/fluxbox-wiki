fda397b81587a8c2b26f4ac184bef1a49ce0c31c
sillyslux <sillyslux@net.hr>
2017-04-14 05:39:08 +0200

    cleanup PageIndex

--- custom/PageIndex.jsx
+++ custom/PageIndex.jsx
@@ -1,45 +1,57 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import pageInfo from 'utils/pageInfo'
-import { pages } from 'config'
-import { prefixLink } from 'gatsby-helpers'
-import styles from 'css/pageindex.module.sass'
-
-import { Link } from 'react-router'
 import { Panel, ListGroup, ListGroupItem } from 'react-bootstrap-externaljs'
+import { Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
+import { pages } from 'config'
+import pageInfo from 'utils/pageInfo'
+
+import styles from 'css/pageindex.module'
+
 
 export default class PageIndex extends Component {
   render () {
     const { language } = pageInfo(this.props.location.pathname)
+
+    // find all pages in /pages/WikiPages
     const indexedPages = pages.filter(a => /WikiPages\/.+\/.?/.test(a.requirePath)&&a.file.name===language).sort((a, b) => a.data.title > b.data.title)
-    const index = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(c => ({ index: c, pages: [] }))
+
+    let index = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(letter => ({ letter, pages: [] }))
+
+    // alphabetically group pages
     indexedPages.forEach(p => index[p.data.title[0].toLowerCase().charCodeAt(0)-97].pages.push(p))
-    const arr = index.map(i => (i.pages.length ? <a key={i.index} href={`#${i.index}`}>{i.index}</a> : <span style={{ opacity: 0.3 }} key={i.index}>{i.index}</span>))
+
+    // build anchor-links to pagelink-groups
+    const anchorLinks = index.map(i => (i.pages.length ?
+      <a key={i.letter} href={`#${i.letter}`}>{i.letter}</a>
+    :
+      <span key={i.letter} style={{ opacity: 0.3 }}>{i.letter}</span>
+    ))
+
+    // filter empty groups from index
+    index = index.filter(letter => letter.pages.length)
+
     return (
-      <Panel header={arr} className={styles.pageindex}>
-        {index.map(o => (
-          o.pages.length ?
-            <Panel header={<h4 id={o.index}>{o.index}</h4>}>
-              <ListGroup>
-                {o.pages.map(p =>
-                  <ListGroupItem>
-                    <Link to={prefixLink(p.path)}>
-                      {p.data.title}
-                    </Link>
-                  </ListGroupItem>,
-                )}
-              </ListGroup>
-            </Panel>
-          :
-            ''
-        ))}
+      <Panel header={anchorLinks} className={styles.pageindex}>
+        {index.map(letterGroup =>
+          <Panel header={<h4 id={letterGroup.letter}>{letterGroup.letter}</h4>}>
+            <ListGroup>
+              {letterGroup.pages.map(page =>
+                <ListGroupItem>
+                  <Link to={prefixLink(page.path)}>
+                    {page.data.title}
+                  </Link>
+                </ListGroupItem>,
+              )}
+            </ListGroup>
+          </Panel>,
+        )}
       </Panel>
     )
   }
 }
-// .panel-heading {display:flex;justify-content:space-between}
-// links {padding: 4px 8px;}
+
 PageIndex.propTypes = {
   location: PropTypes.shape({
     pathname: PropTypes.string,
