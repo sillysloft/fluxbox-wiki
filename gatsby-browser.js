e317b235a5477dc0494afbf1ccf053c74173704e
sillyslux <sillyslux@net.hr>
2017-04-14 15:40:25 +0200

    remove debugger, eslint-disable no-new in gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,11 @@
 /* global document, ScrollSpy */
+/* eslint-disable no-new */
 import { ScrollSpy, Affix } from './static/bootstrap-native.min.js'
 
 let onRouteUpdate = () => {}
 
 if (typeof document !== 'undefined' && process.env.NODE_ENV === 'development') {
   onRouteUpdate = () => {
-    debugger
     delete document.body.ScrollSpy
     if (document.querySelector('#spytoc')) {
       new ScrollSpy(document.body, {
