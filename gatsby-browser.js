6114c2e9fdf6e8b29b85722ef32575b4b5ceb7ad
sillyslux <sillyslux@net.hr>
2017-04-14 13:11:53 +0200

    remove setTimeout from gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,16 +5,15 @@ let onRouteUpdate = () => {}
 
 if (typeof document !== 'undefined' && process.env.NODE_ENV === 'development') {
   onRouteUpdate = () => {
-    setTimeout(() => {
-      delete document.body.ScrollSpy
-      if (document.querySelector('#spytoc')) {
-        new ScrollSpy(document.body, {
-          target: document.querySelector('#spytoc'),
-          offset: 80,
-        }).refresh()
-        new Affix(document.getElementById('spytoc'))
-      }
-    }, 500)
+    debugger
+    delete document.body.ScrollSpy
+    if (document.querySelector('#spytoc')) {
+      new ScrollSpy(document.body, {
+        target: document.querySelector('#spytoc'),
+        offset: 80,
+      }).refresh()
+      new Affix(document.getElementById('spytoc'))
+    }
   }
 }
 
