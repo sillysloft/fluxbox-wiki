8689d7a748d40ab800796a9b8b8a177327ef6b58
sillyslux <sillyslux@net.hr>
2017-04-24 02:40:24 +0200

    update editor link in footer

--- custom/Footer.jsx
+++ custom/Footer.jsx
@@ -10,15 +10,15 @@ import styles from 'css/footer.module.sass'
 export default class Footer extends Component {
   openEditWindow (evt) {
     evt.preventDefault()
-    window.open('/edit/', '_blank', 'toolbar=no,menubar=no,titlebar=no,height=600,width=800')
+    window.open('/editor/', 'EditorWindow', 'toolbar=no,menubar=no,titlebar=no,height=600,width=800')
   }
   render () {
     const { page } = pageInfo(this.props.location.pathname)
     return (
-      <div className={styles.footer}>
+      <div className={`${styles.footer} footer`}>
         fluxbox-wiki.org
         <a style={{ float: 'right' }} href={`https://github.com/sillyslux/fluxbox-wiki/blob/master/pages/${page.requirePath}`}>edit at github</a>
-        <a style={{ float: 'left' }} href="/live-editor" onClick={this.openEditWindow}>eddy</a>
+        <a style={{ float: 'left' }} href="/editor/" onClick={this.openEditWindow}>eddy</a>
       </div>
     )
   }
